1eb08d32c44cf672a40993dd1bcd0c4e6015de3f
gaojizhou <gaojizhou@qq.com>
2021-10-03 10:11:45 +0800

    select gfirewalls potr

--- run.bash
+++ run.bash
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-# Make sure only root can run our script
+# run in root
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
+   echo "this script must be run as root or sudo permissions" 1>&2
    exit 1
 fi
 
+read -p "select gfirewalls potr (1000-9999): " PORT
+until
+[ $PORT -gt 999 -a $PORT -lt 10000 ]
+do
+read -p "$PORT value error, please enter number between 1000 to 9999: " PORT
+done
+
+# backup user.rules 
+
+# check ufw 
+
+# check java
+
+# run jar
+java -jar ./target/gfirewalls.jar --server.port=$PORT
