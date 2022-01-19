c76696afa0afb34a05a0c5b586feaf64a83dd245
gaojizhou <gaojizhou@qq.com>
2022-01-19 10:16:29 +0800

    releases bug fix

--- tar.sh
+++ tar.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 rm -rf ./target &&  mvn clean package -Dmaven.test.skip=true
-mkdir ./target/gfirewalls
-cp ./README.md ./target/gfirewalls
-cp ./run.bash ./target/gfirewalls
-cp ./target/gfirewalls.jar ./target/gfirewalls
-tar -cvf ./target/gfirewalls.tar ./target/gfirewalls
+mkdir ./gfirewalls
+cp ./README.md ./gfirewalls
+cp ./run.sh ./gfirewalls
+cp ./target/gfirewalls.jar ./gfirewalls
+tar -cvf ./target/gfirewalls.tar ./gfirewalls
+rm -r ./gfirewalls
